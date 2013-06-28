2ef82c6144f15a9c37efde91abfbdfb2f669779f
Jonas Baumann <jone@jone.ch>
2013-06-28 11:28:51 +0200

    Create package.

---
+++ README.rst
@@ -0,0 +1,22 @@
+ftw.builder
+===========
+
+Create Plone objects in tests with the
+`Builder Pattern <http://www.oodesign.com/builder-pattern.html>`_.
+
+
+Links
+-----
+
+- Main github project repository: https://github.com/4teamwork/ftw.builder
+- Issue tracker: https://github.com/4teamwork/ftw.builder/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.builder
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.builder
+
+
+Copyright
+---------
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.builder`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.0.0.dev0'
+
+tests_require = [
+    'unittest2',
+    ]
+
+
+setup(name='ftw.builder',
+      version=version,
+      description='Builder pattern for creating Plone objects in tests',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw builder plone',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.builder',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', ],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
+
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      )
